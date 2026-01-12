d6088a2713077f22fc08f3c378e2dfb81327172a
Ayoub Makrini <ayoubmakrini@outlook.com>
2026-01-12 13:54:52 +0100

    inschrijvingen aangepast en ingevoerd

--- app/Models/Inschrijving.php
+++ app/Models/Inschrijving.php
@@ -18,6 +18,13 @@ class Inschrijving extends Model
         'keuzedeel_id',
         'periode',
         'status',
+        'motivatie',
+        'voorkennis',
+        'telefoon',
+        'niveau',
+        'leerdoelen',
+        'aanwezig_info',
+        'opmerkingen',
     ];
 
     public function user()
